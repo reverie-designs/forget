62f0638bc8899ed82bb596048880e518b2567b84
reverie-designs <dfrumovitch@gmail.com>
2020-01-06 16:45:12 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './forgetmenot.png';
+import './App.scss';
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Forget Me Not
         </p>
         <a
           className="App-link"
