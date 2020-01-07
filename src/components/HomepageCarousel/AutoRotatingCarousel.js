a5ec1b21eb91c4b165aabdc4b5bbab74a6182ae9
Fatima Altaf <fatima.altaf@gmail.com>
2020-01-07 13:36:03 -0500

    add colors and working carousel

--- src/components/HomepageCarousel/AutoRotatingCarousel.js
+++ src/components/HomepageCarousel/AutoRotatingCarousel.js
@@ -262,7 +262,7 @@ class AutoRotatingCarousel extends Component {
 
 AutoRotatingCarousel.defaultProps = {
   autoplay: true,
-  interval: 6000,
+  interval: 4000,
   mobile: false,
   open: false,
   hideArrows: false
--- src/components/HomepageCarousel/HomepageCarousel.js
+++ src/components/HomepageCarousel/HomepageCarousel.js
@@ -2,51 +2,52 @@ import React, { useState} from 'react';
 
 import AutoRotatingCarousel from './AutoRotatingCarousel';
 import Slide from './Slide';
-import { red, blue, green } from '@material-ui/core/colors';
+import { blue, lime, cyan } from '@material-ui/core/colors';
 
 export default function HomepageCarousel() {
+  
+const [carouselState, setCarouselState] = useState(
+  {open: true 
+  })
 
-  const [state, setState] = useState(true)
-
-  return (
-
-    <div style={{ position: 'relative', width: '100%', height: 500 }}>
-      <AutoRotatingCarousel
-        open={state.open}
-        onClose={() => setState({ open: false })}
-        onStart={() => setState({ open: false })}
-        style={{ position: 'absolute' }}
-      >
-         <Slide
-          media={<img src='https://image.flaticon.com/icons/svg/843/843220.svg' />}
-          aria-label="Grandmother and grandfather standing together"
-          mediaBackgroundStyle={{ backgroundColor: red[400] }}
-          style={{ backgroundColor: red[600] }}
-          title='This is a very cool feature'
-          subtitle='Just using this will blow your mind.'
-        />
-        <Slide
-          media={<img src='https://image.flaticon.com/icons/svg/843/843220.svg' />}
-          mediaBackgroundStyle={{ backgroundColor: red[400] }}
-          style={{ backgroundColor: red[600] }}
-          title='This is a very cool feature'
-          subtitle='Just using this will blow your mind.'
-        />
-        <Slide
-          media={<img src='https://image.flaticon.com/icons/svg/854/854878.svg' />}
-          mediaBackgroundStyle={{ backgroundColor: blue[400] }}
-          style={{ backgroundColor: blue[600] }}
-          title='Ever wanted to be popular?'
-          subtitle='Well just mix two colors and your are good to go!'
-        />
-        <Slide
-          media={<img src='https://image.flaticon.com/icons/svg/123/123392.svg' />}
-          mediaBackgroundStyle={{ backgroundColor: green[400] }}
-          style={{ backgroundColor: green[600] }}
-          title='May the force be with you'
-          subtitle='The Force is a metaphysical and ubiquitous power in the Star Wars fictional universe.'
-        />
-      </AutoRotatingCarousel>
-    </div>
+return (
+<div style={{ position: 'relative', width: '100%', height: 500 }}>
+  <AutoRotatingCarousel
+    label='Sign Up'
+    open={carouselState.open}
+    onClose={() => setCarouselState({ open: false })}
+    onStart={() => setCarouselState({ open: false })}
+    style={{ position: 'absolute' }}
+  >
+     <Slide
+      media={<img src='https://image.flaticon.com/icons/svg/122/122903.svg' />}
+      mediaBackgroundStyle={{ backgroundColor: cyan[400] }}
+      style={{ backgroundColor: cyan[600] }}
+      title='Forget Me Not'
+      subtitle='The ideal solution to assist you in helping your family member with dementia continue to live an independent life.'
+    />
+     <Slide
+      media={<img src='https://image.flaticon.com/icons/svg/854/854878.svg' />}
+      mediaBackgroundStyle={{ backgroundColor: lime[400] }}
+      style={{ backgroundColor: lime[600] }}
+      title="Current location"
+      subtitle='You can also set a geofence for safety!'
+    />
+    <Slide
+      media={<img src='https://image.flaticon.com/icons/svg/843/843220.svg' />}
+      mediaBackgroundStyle={{ backgroundColor: cyan[400] }}
+      style={{ backgroundColor: cyan[600] }}
+      title='Create reminders and appointments'
+      // subtitle='reminders and appointments'
+    />
+    <Slide
+      media={<img src='https://image.flaticon.com/icons/svg/123/123392.svg' />}
+      mediaBackgroundStyle={{ backgroundColor: lime[400] }}
+      style={{ backgroundColor: lime[600] }}
+      title='View all notifications you created'
+      subtitle='Calendar for easy viewing'
+    />
+  </AutoRotatingCarousel> 
+  </div>
   )
 }
