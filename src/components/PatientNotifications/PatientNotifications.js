aa33264cf9a541a13c1874a61b00cbc908656b5e
Fatima Altaf <fatima.altaf@gmail.com>
2020-01-11 12:28:00 -0500

    initial commit

---
+++ src/components/PatientNotifications/PatientNotifications.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
+
+const data = {
+  notification: [{
+    id: 1,
+    date: "Jan 11 2020",
+    time: "16:00:00",
+    pills: false,
+    appointment: true,
+    food: true,
+    patient: true
+  },
+  {
+    id: 2,
+    date: "Jan 11 2020",
+    time: "12:45:00",
+    pills: false,
+    appointment: true,
+    food: true,
+    patient: true
+  }
+]
+}
+
+const checkPatientTime = () => {
+  const today = new Date()
+  const patientCurrentTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  if (patientCurrentTime === data.notification.time) {
+    return data.notification
+  }
+}
+
+function PatientNotification(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
+export default function CustomizedSnackbars() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Button variant="outlined" onClick={handleClick}>
+        Open success snackbar
+      </Button>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <PatientNotification onClose={handleClose} severity="success">
+          This is a success message!
+        </PatientNotification>
+      </Snackbar>
+    </div>
+  );
+}
