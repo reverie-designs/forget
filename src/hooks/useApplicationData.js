4ef592904b16a0d612733c116888e5b377215456
reverie-designs <dfrumovitch@gmail.com>
2020-01-12 18:08:34 -0500

    initial imports for useApplicationData

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,5 @@
+import React, {useEffect, useReducer} from "react"
+// import axios from "axios"
+import reducer, {SET_USER, SET_NOTIFICATIONS_DATA,SET_NOTIFICATIONS_DAY, SET_SETTINGS} from "../reducers/application"
+
+
