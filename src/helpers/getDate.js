743eb614898e820ff2818fed58b87cafb1451198
reverie-designs <dfrumovitch@gmail.com>
2020-01-12 17:28:39 -0500

    import date spliting function to helpers

---
+++ src/helpers/getDate.js
@@ -0,0 +1,14 @@
+// // import React from 'react';
+
+
+//   const splitDate =(fullDate) =>{
+//     const dateArray = fullDate.toString().split(" ");
+//     const date = [];
+//     date.push(dateArray[0]);
+//     date.push(dateArray[1]);
+//     date.push(dateArray[2]);
+//     date.push(dateArray[3]);
+//     return date.join(" ");
+//   }
+
+//   export default splitDate{return splitDate}
