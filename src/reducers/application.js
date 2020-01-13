51bec5ccecb204cc69877ed0ff5eda17c77f21e5
reverie-designs <dfrumovitch@gmail.com>
2020-01-12 20:01:36 -0500

    added application switch reducer

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -2,10 +2,11 @@ export const SET_USER = "SET_USER"
 export const SET_NOTIFICATIONS_DATA = "SET_NOTIFICATIONS_DATA"
 export const SET_NOTIFICATIONS_DAY = "SET_NOTIFICATIONS_DAY"
 export const SET_SETTINGS = "SET_SETTINGS"
+export const SET_ERROR = "SET_ERROR"
 
 
 export default function reducer(state, action) {
-  
+
   switch (action.type) {
 
     case SET_USER:
@@ -15,10 +16,13 @@ export default function reducer(state, action) {
       return { ...state, notifications: action.notifications}
 
     case SET_NOTIFICATIONS_DAY: 
-      return {...state, todays_notifications}
+      return {...state, todays_notifications: action.todays_notifications}
     
     case SET_SETTINGS:
-      return {...state, settings}
+      return {...state, settings: action.settings}
+
+    case SET_ERROR:
+          return {...state, error: action.error}  
 
     default:
       throw new Error(
