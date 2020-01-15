63e6acc795bab36722262d95675c32178e02b94d
Fatima Altaf <fatima.altaf@gmail.com>
2020-01-15 16:16:20 -0500

    realign icons in side menu

--- src/components/DisableGeofence.js
+++ src/components/DisableGeofence.js
@@ -14,6 +14,7 @@ export default function GeofenceToggleButton(props) {
             onChange={props.onChange}
             value={props.checked}
             color="primary"
+            className={props.className}
           />
         }
         label="ON"
--- src/components/DisableNotification.js
+++ src/components/DisableNotification.js
@@ -14,6 +14,7 @@ export default function NotificationToggleButton(props) {
             onChange={props.onChange}
             value={props.checked}
             color="primary"
+            className={props.className}
           />
         }
         label="Off"
